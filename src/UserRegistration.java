509b99bf2d584d34d0d0ae6c74129c5bfad899e8
Puja Borse <pujaborse13@gmail.com>
2024-12-02 14:16:51 +0530

    Validate Email Id

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -3,46 +3,25 @@ import java.util.regex.Pattern;
 
 public class UserRegistration
 {
-    public static boolean isValidFirstName(String firstName)
-    {
-        String regex = "^[A-Z][a-zA-Z]{2,}$";
-        return Pattern.matches(regex, firstName);
+    public static boolean isValidEmail(String email) {
+        String regex = "^[a-zA-Z0-9]+([._][a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\\.[a-zA-Z]{2,}){1,2}$";
+        return Pattern.matches(regex, email);
     }
-
-    public static boolean isValidLastName(String lastName) {
-        String regex = "^[A-Z][a-zA-Z]{2,}$";
-        return Pattern.matches(regex, lastName);
-    }
-
-
 
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Git User Registration System!");
-        System.out.print("Enter your First Name: ");
-        String firstName = scanner.nextLine();
 
-        if (isValidFirstName(firstName)) {
-            System.out.println("First Name is valid!");
-        }
+        System.out.print("Enter your Email Address: ");
+        String email = scanner.nextLine();
 
-        else {
-            System.out.println("Invalid First Name. Please ensure it starts with a capital letter and has at least 3 characters.");
-
-        }
-
-
-        System.out.print("Enter your Last Name: ");
-        String lastName = scanner.nextLine();
-
-        if (isValidLastName(lastName)) {
-            System.out.println("Last Name is valid!");
+        if (isValidEmail(email)) {
+            System.out.println("Email is valid!");
         } else {
-            System.out.println("Invalid Last Name. Please ensure it starts with a capital letter and has at least 3 characters.");
+            System.out.println("Invalid Email. Please ensure it follows the format abc.xyz@bl.co.in with proper @ and . positions.");
         }
-
 
     }
 }
