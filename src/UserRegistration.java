27bfa904081daf8135980f15d9a53de09b15488b
Puja Borse <pujaborse13@gmail.com>
2024-12-02 14:34:47 +0530

    Validate Password Rule-1

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -3,9 +3,10 @@ import java.util.regex.Pattern;
 
 public class UserRegistration
 {
-    public static boolean isValidMobileNumber(String mobile) {
-        String regex = "^[0-9]{1,3}\\s[0-9]{10}$";
-        return Pattern.matches(regex, mobile);
+    public static boolean isValidPassword(String password) {
+        String regex = "^.{8,}$";
+        return Pattern.matches(regex, password);
+
 
     }
 
@@ -15,13 +16,13 @@ public class UserRegistration
 
         System.out.println("Welcome to the Git User Registration System!");
 
-        System.out.print("Enter your Mobile Number: ");
-        String mobile = scanner.nextLine();
+        System.out.print("Enter your Password: ");
+        String password = scanner.nextLine();
 
-        if (isValidMobileNumber(mobile)) {
-            System.out.println("Mobile Number is valid!");
+        if (isValidPassword(password)) {
+            System.out.println("Password is valid!");
         } else {
-            System.out.println("Invalid Mobile Number. Please ensure it follows the format '91 9919819801' with a country code and 10 digits.");
+            System.out.println("Invalid Password. Please ensure it has a minimum of 8 characters.");
         }
 
 
