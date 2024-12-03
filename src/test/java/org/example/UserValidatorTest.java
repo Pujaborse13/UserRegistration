42ec6a3ac0c41071eb2ef960d37ac6896b09073f
Puja Borse <pujaborse13@gmail.com>
2024-12-03 15:53:01 +0530

    Validate Parameterised test cases

--- src/test/java/org/example/UserValidatorTest.java
+++ src/test/java/org/example/UserValidatorTest.java
@@ -8,8 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
-public class UserValidatorTest {
-
+public class UserValidatorTest{
 
     @Test
     public void givenValidFirstName_HappyCase(){
