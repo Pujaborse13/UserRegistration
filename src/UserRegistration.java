52e7953723143326ec8f44155eccf98c181a07da
Puja Borse <pujaborse13@gmail.com>
2024-12-03 11:42:00 +0530

    Validate Password Rule-9

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -10,7 +10,6 @@ public class UserRegistration
         return Pattern.matches(regex, password);
 
     }
-
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
